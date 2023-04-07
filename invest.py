262378eff52610ce47cd6b85b3111a6ace970225
ugwuejeh <ugwudavidejeh@gmail.com>
2023-04-07 15:51:53 +0100

    Update invest.py

--- invest.py
+++ invest.py
@@ -1,6 +1,7 @@
+
 def createinvestment(): 
  
-  bonus = 0.5
+  bonus = 0.5("50% of your investment")
   investor_name = input("enter your name, ")
   amount_invested = int(input("enter amount to invest, "))
   investment_bonus = amount_invested * bonus
@@ -8,10 +9,10 @@ def createinvestment():
   print(investor_name, "your wallet balance is", "#",  wallet_balance)
   
   withdrawer_amount = int(input("enter amount to withdraw, "))
-  tax = 0.07
+  tax = 0.07("7% VAT on each withdraw")
   withdrawer_tax = withdrawer_amount * tax
-  total_withdrawer = withdrawer_amount - withdrawer_tax
-  New_wallet_balance = wallet_balance - total_withdrawer
+  total_withdrawer = withdrawer_amount 
+  New_wallet_balance = wallet_balance - total_withdrawer - withdrawer_tax
 
   if wallet_balance > total_withdrawer:
 
@@ -20,4 +21,3 @@ def createinvestment():
      print("you have insufficient balance.")
     
 createinvestment()
-
